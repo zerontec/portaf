272aede74be0855eb4dfd93f402016e06109f452
zerontec <zerontecnologia1@gmail.com>
2022-01-11 23:50:28 -0430

    efecto poligono en intro y add estilos del mismo en intro css

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -6,30 +6,33 @@ const Intro = () => {
 
 return (
   <div className="i">
-    <div className="i-left">Left </div>
+    <div className="i-left">
     <div className="i-left-wrapper">
       <h2 className="i-intro">My Nombre es </h2>
       <h1 className="i-name">Leoberto Zeron </h1>
       <div className="i-title">
         <div className="i-title-wrapper">
-          <div className="i-title-item">Web Developer</div>
+          <div className="i-title-item ">Web Developer</div>
 
           <div className="i-title-item">UI/UX Designer</div>
 
           <div className="i-title-item">Escritor</div>
 
           <div className="i-title-item">Web Developer</div>
+          <div className="i-title-item">Web Developer</div>
         </div>
       </div>
+      
 
-      <div className="i-desc">
+      <p className="i-desc">
           Soy Desarrollador Full Stack 
-      </div>
+      </p>
+    </div>
     </div>
 
     <div className="i-rigth">
         <div className="i-bg"></div>
-        <img src={Code} alt="" className='i-img' />    
+      
     </div>
   </div>
 );
